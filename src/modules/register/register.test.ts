8b5104b1bac3a5474ecf1b6bf87c3e4b2fc065c6
Andyparr <Andy_parr@me.com>
2018-05-01 15:58:30 +0100

    Add error handling

--- src/modules/register/register.test.ts
+++ src/modules/register/register.test.ts
@@ -11,21 +11,30 @@ beforeAll(async () => {
   getHost = () => `http://127.0.0.1:${port}`
 })
 
-const email = 'tom@bob.com'
-const password = 'jalksdf'
+const email = 'test@email.com'
+const password = 'testpassword'
 
 const mutation = `
 mutation {
-  register(email: "${email}", password: "${password}")
+  register(email: "${email}", password: "${password}") {
+    path
+    message
+  }
 }
 `
 
 test('Register user', async () => {
   const response = await request(getHost(), mutation)
-  expect(response).toEqual({ register: true })
+  expect(response).toEqual({ register: null })
   const users = await User.find({ where: { email } })
   expect(users).toHaveLength(1)
   const user = users[0]
   expect(user.email).toEqual(email)
   expect(user.password).not.toEqual(password)
+})
+
+test('Register user with same email', async () => {
+  const response2: any = await request(getHost(), mutation)
+  expect(response2.register).toHaveLength(1)
+  expect(response2.register[0].path).toEqual('email')
 })
